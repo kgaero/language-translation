3046b5474d3cf5292b124efcafab8597d864aceb
kgaero <kgaero+github@gmail.com>
2026-02-18 12:30:52 +0530

    Update model.py

--- model.py
+++ model.py
@@ -73,7 +73,44 @@ class MultiheadattentionBlock(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.h = h
-        self.dropout = dropout
 
         assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model / h
+
+        self.w_q = nn.Linear(d_model,d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, q, k, v, mask):
+
+
+        query = self.w_q(q)
+        key   = self.w_k(k)
+        value = self.w_v(v)
+
+        # (Batch, Seq_length, d_model) -> (Batch, Seq_Len, h, d_k) --> (Batch, h, Seq_Len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
+
+        x, self.attention_scores = MultiheadattentionBlock.attention(query, key, value, mask,self.dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        atention_scores = (query @key.transpose(-2,-1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1)
+
+
+
+
 
